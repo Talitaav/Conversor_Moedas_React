2acb0cc93ece265f65a46aac52fb492586df70e3
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-11-28 19:48:57 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ConversorDeMoedas from './components/ConversorDeMoedas'
+
+function App() {
+
+
+  return (
+    <>
+      <ConversorDeMoedas />
+    </>
+  )
+
+}
+
+export default App
